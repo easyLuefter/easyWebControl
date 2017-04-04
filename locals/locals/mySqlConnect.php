80c53411741e81f0b303ad2c873430f7bb85fb95
easyLuefter <info@easyluefter.ch>
2017-04-04 14:30:03 +0200

    dulicate

--- locals/locals/mySqlConnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$con = mysql_connect("localhost","root","atmel011");
-if (!$con) {
-	echo date("d.m.Y H:i:s") . "Could not connect: " . mysql_error() . "\n";
-}
-mysql_select_db("easyLuefter", $con);
-
-?>
