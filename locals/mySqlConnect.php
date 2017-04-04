2a9065c2d594ef7537f432aa6f03d44264578a68
easyLuefter <info@easyluefter.ch>
2017-04-04 14:37:26 +0200

    Update mySqlConnect.php

--- locals/mySqlConnect.php
+++ locals/mySqlConnect.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysql_connect("localhost","root","atmel011");
+$con = mysql_connect("localhost","root","pw");
 if (!$con) {
 	echo date("d.m.Y H:i:s") . "Could not connect: " . mysql_error() . "\n";
 }
