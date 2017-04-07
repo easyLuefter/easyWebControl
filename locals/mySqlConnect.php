0d0f9edb962bd8b01efdd56d5ec91c0297a525c1
easyLuefter <info@easyluefter.ch>
2017-04-07 09:17:38 +0200

    create db if not exist added

--- locals/mySqlConnect.php
+++ locals/mySqlConnect.php
@@ -4,6 +4,7 @@ $con = mysql_connect("localhost","root","pw");
 if (!$con) {
 	echo date("d.m.Y H:i:s") . "Could not connect: " . mysql_error() . "\n";
 }
+mysql_query("CREATE DATABASE IF NOT EXISTS easyLuefter");
 mysql_select_db("easyLuefter", $con);
 
 ?>
