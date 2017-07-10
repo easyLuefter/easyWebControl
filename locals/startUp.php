3ce2c0c40523d9c1b3711c00e664f7c6fd27321d
easyLuefter <info@easyluefter.ch>
2017-07-10 19:21:14 +0200

    new startUp.php

--- locals/startUp.php
+++ locals/startUp.php
@@ -18,11 +18,12 @@ if (!flock($fp, LOCK_EX | LOCK_NB)) {
     exit;
 } else echo "$PHPname not locked\n";
 
-
 	echo shell_exec("sudo pigpiod");
+	echo shell_exec("$path/../dht22/easydht 0 >/dev/null 2>&1 &");
 	//echo shell_exec("sudo pigs hp 12 23000 200000");
 	//echo shell_exec("sudo pigs hp 13 23000 200000");
 	echo shell_exec("php $path/../getHumTmp.php d >/dev/null 2>&1 &");
 	echo shell_exec("php $path/../regler.php d >/dev/null 2>&1 &");
 	echo shell_exec("php $path/../humTemp.php d >/dev/null 2>&1 &");
+	
 ?>
